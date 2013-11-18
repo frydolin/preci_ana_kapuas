5eafb99b4dc2bf19b427b80f05bf6194da24aa1a
Fridolin@top <fridolinhaag@yahoo.de>
2013-11-18 17:49:34 +0100

    change colors

--- scripts/plot.R
+++ scripts/plot.R
@@ -4,8 +4,18 @@
 
 # COLOR SCHEME #
   #currently for 11
-  hexcolors=c("#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00" ,"#CAB2D6","#6A3D9A", "#FFFF99") 
-
+  hexcolors=c(#"#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00" ,"#CAB2D6","#6A3D9A", "#FFFF99") 
+    "#FF7600",
+    "#01939A",
+    "#A30008",
+    "#0D8800",
+    "#7F054A",
+    "#649305",
+    "#6D14A2",
+    "#17599E",
+    "#0EC20E",
+    "#F31D11",
+    "#0B9B7F")
 #### Correlograms ####
 library(corrgram)
 source("scripts/functions.R") #functions such as mdf,corgr,.. . As defined in the file.
