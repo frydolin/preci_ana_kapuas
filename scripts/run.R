78fc08c2e6f51967a37a366afb1af355459ad65d
Fridolin@top <fridolinhaag@yahoo.de>
2013-12-21 12:12:59 +0100

    *rename files

--- scripts/run.R
+++ scripts/run.R
@@ -4,9 +4,9 @@
 
 # Maybe you need to set the correct correct wd!: set.wd()
 
-source("scripts/load.R") 	    ## load.R loads in all the data and converts to appropriate formats ##
-source("scripts/aggregate.R")	## summaries.R creates all necessary summaries and variables to be analysed ##
-source("scripts/ts_plot.R")	  ## ts_plot.R plots summaries to output ##
-source("scripts/sp_plot.R")   ## sp_plot.R plots comperative summaries to output ##
+source("scripts/load.R") 	    ## loads in all the data and converts to appropriate formats ##
+source("scripts/aggregate.R")	## creates all necessary summaries and variables to be analysed ##
+source("scripts/analyse.R")	  ## plots summaries to output ##
+source("scripts/compare.R")   ## plots comparative summaries to output ##
 
 ########## END run R#############
