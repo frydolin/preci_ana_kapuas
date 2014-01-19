ab6da5a658472935fd411ced72a552b7cc410f9b
Fridolin@top <fridolinhaag@yahoo.de>
2014-01-19 12:33:14 +0100

    * corrections of hom tests

--- scripts/homogeneity_tests.R
+++ scripts/homogeneity_tests.R
@@ -13,7 +13,7 @@ neumann.ratio=function(x, na.rm=TRUE){
   #Convert to vector of zoo objects, because rank() needs that
   if (is.zoo(x)){x=as.vector(x)}
   
-  n    <-sum(!is.na(x)) 
+  n    <-sum(!is.na(x)) #just for output illustration
   x_mean= (mean(x, na.rm=na.rm))
   ind.length= length(x)
   x.plusone= x[2:ind.length]
@@ -23,17 +23,20 @@ neumann.ratio=function(x, na.rm=TRUE){
 }
 
 buishand.test=function(x, na.rm=TRUE){
+   n    <-length(x)       #index length
+   nn   <-sum(!is.na(x))  #number of valid values
+   if (na.rm==FALSE && n>nn ) {stop("Data contains missing values, 
+                             set na.rm to TRUE if you want to use the function anyway")}
    x_mean=mean(x, na.rm=na.rm)
-   n    <-sum(!is.na(x)) 
    dif=x-x_mean
    Sk=numeric()
    Sk[0]=0
-   for(k in (1:n)){Sk[k]=sum(dif[1:k], na.rm=na.rm)}
+   for(k in (1:n)){Sk[k]=sum(dif[1:k], na.rm=na.rm)}  #sum up over whole index
    rSk=(Sk/sd(x, na.rm=na.rm)) #rescaled adjusted partial sums
    R=max(rSk, na.rm=na.rm)-min(rSk, na.rm=na.rm)
    Q=max(abs(rSk), na.rm=na.rm)
-   R.sign=R/(sqrt(n))
-   Q.sign=Q/(sqrt(n))
+   R.sign=R/(sqrt(nn))
+   Q.sign=Q/(sqrt(nn))
    break.points=which(abs(rSk)==Q)
    if (R.sign==0){R.sign=NA}
    return(list("n"=n,"rSk"=rSk, "abs.Max"=Q, "R.sign"=R.sign, "breakpoints"=break.points))
@@ -45,34 +48,35 @@ pettitt.test=function(x, na.rm=TRUE){
   if (!is.zoo(x)&&!is.vector(x)) stop("Invalid argument: 'class(x)' must be in c('vector' or 'zoo')")
   #Convert to vector of zoo objects, because rank() needs that
   if (is.zoo(x)){x=as.vector(x)}
-  n       <-sum(!is.na(x))
-  x.rank=rank(x, ties.method="average", na.last=na.rm)
+  
+  n    <-length(x)       #index length
+  nn   <-sum(!is.na(x))  #number of valid values
+  x.rank=rank(x, ties.method="average", na.last=NA)
   Xk=numeric()
   for(k in (1:n)){
-    sum.part=numeric()
-    for(i in (1:k)){sum.part[i]=x.rank[i]}
-  Xk[k]=2*(sum(sum.part, na.rm=na.rm))-k*(n+1)
-    rm(sum.part)
-  }  
+    sum.part=x.rank[1:k]
+    Xk[k]=2*(sum(sum.part))-k*(nn+1)
+    }  
   Xe=max(abs(Xk))
   position=which(abs(Xk)==Xe)
   return(list("n"=n, "X_k"=Xk, "X_e"=Xe, "breakpoints"=position))
 }
 
 snh.test=function(x, na.rm=TRUE){
-  n       <-sum(!is.na(x))  #missing values consequently removed
-  mean_x  <-mean(x, na.rm=na.rm)
-  sd_x    <-sd(x, na.rm=na.rm)
+  if(na.rm==TRUE){x=x[!is.na(x)]} #remove NAs, because of scaling with n, this is the best way?!
+  n    <-length(x)       #index length
+  mean_x  <-mean(x)
+  sd_x    <-sd(x)
   dif  <-(x-mean_x)/sd_x #Vector of differences between value and mean, scaled by sd
   z_1=numeric()
   z_2=numeric()
   T_k=numeric()
   for (k in 1:n){
-  z_1[k]=(1/k)*sum(dif[1:k], na.rm=na.rm) #sum up the vector
-  z_2[k]=(1/(n-k))*sum(dif[(k+1):n],  na.rm=na.rm)
+  z_1[k]=(1/k)*sum(dif[1:k]) #sum up the vector
+  z_2[k]=(1/(n-k))*sum(dif[(k+1):n])
   T_k[k] <- k*(z_1[k])^2+(n-k)*(z_2[k])^2 # k=1...n
   }
-  T_0=max(T_k, na.rm=na.rm)
+  T_0=max(abs(T_k), na.rm=TRUE)
   if (T_0==0){T_0=NA} #set to NA 
   return(list("n"=n, "T_k"=T_k, "T_0"=T_0))
 }
